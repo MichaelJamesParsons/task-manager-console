a71b684b041b239388af8d1ca058405b9bbf5b58
Michael-James Parsons <mjay.parsons@gmail.com>
2016-12-10 14:18:49 -0500

    Initial commit.

---
+++ src/main/java/TaskManagerConsole.java
@@ -0,0 +1,240 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.commons.cli.*;
+
+public class TaskManagerConsole {
+    private Gson jsonParser;
+    private Options options;
+    private static final String API_ENDPOINT = "http://138.197.15.79/task";
+
+    public static void main(String[] args) {
+        TaskManagerConsole taskManager = new TaskManagerConsole();
+        taskManager.execute(args);
+    }
+
+    private TaskManagerConsole() {
+        options = initializeCommandlineOptions();
+        jsonParser = new Gson();
+    }
+
+    private void execute(String[] args) {
+        try {
+            CommandLine cli = initializeCli(args);
+            Option[] parsedOptions = cli.getOptions();
+
+            if(parsedOptions.length == 0) {
+                dieWithHelper();
+            } else if(parsedOptions.length > 1) {
+                dieWithHelper("ERROR: Only one command may be executed at a time!");
+            }
+
+            processOption(parsedOptions[0]);
+
+        } catch (ParseException e) {
+            dieWithHelper("Invalid argument! " + e.getLocalizedMessage() + ".");
+        }
+    }
+
+    private void processOption(Option option) {
+        String name = option.getArgName();
+
+        if(name.compareTo("new") == 0) {
+            newTask(option.getValues());
+        }
+    }
+
+    private void newTask(String[] args) {
+        String taskName = args[0];
+        boolean isStartable = false;
+
+        if(args.length == 2) {
+            isStartable = Boolean.parseBoolean(args[1]);
+        }
+
+        try {
+            HttpResponse response = Unirest.post(API_ENDPOINT)
+                    .header("Content-Type", "application/json")
+                    .header("accept", "application/json")
+                    .body("{\"description\":\"" + taskName + "\"}")
+                    .asJson();
+
+            JsonObject task = jsonParser.fromJson(response.getBody().toString(), JsonObject.class);
+            System.out.println("Task added with ID " + task.get("id"));
+
+            if(isStartable) {
+
+            }
+        } catch (UnirestException e) {
+            System.out.println("Failed to add new task.");
+        }
+    }
+
+    /**
+     * @todo complete!
+     * @param id
+     */
+    private void startTask(int id) {
+        JsonObject task = getTask(id);
+    }
+
+    /**
+     * Fetch a task by the given ID from the API.
+     *
+     * @param id - The ID of the task to fetch.
+     * @return - The task.
+     */
+    private JsonObject getTask(int id) {
+        try {
+            return sendGetRequest(id + "");
+        } catch (UnirestException e) {
+            die("Failed to load task.");
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * @param endpointSuffix - The route to send the request. (e.g /tasks/all)
+     * @param payload - The json data to send with the request.
+     * @return
+     *
+     * @throws UnirestException
+     */
+    private JsonObject sendPostRequest(String endpointSuffix, String payload) throws UnirestException {
+        HttpResponse response = Unirest.post(API_ENDPOINT + endpointSuffix)
+                .header("Content-Type", "application/json")
+                .header("accept", "application/json")
+                .body(payload)
+                .asJson();
+
+        return deserializeJson(response.getBody().toString());
+    }
+
+    /**
+     * Send a GET request to the API.
+     *
+     * @param endpointSuffix - The route to send the request. (e.g /tasks/all)
+     * @return The API response.
+     *
+     * @throws UnirestException
+     */
+    private JsonObject sendGetRequest(String endpointSuffix) throws UnirestException {
+        HttpResponse response = Unirest.post(API_ENDPOINT + endpointSuffix)
+                .header("Content-Type", "application/json")
+                .header("accept", "application/json")
+                .asJson();
+
+        return deserializeJson(response.getBody().toString());
+    }
+
+    private JsonObject deserializeJson(String json) {
+        return jsonParser.fromJson(json, JsonObject.class);
+    }
+
+    private CommandLine initializeCli(String[] args) throws ParseException {
+        return parseCommandLineOptions(args);
+    }
+
+    /**
+     * Parse the command line parameters given by the user.
+     *
+     * @param args - The arguments submitted by the user.
+     * @return
+     * @throws ParseException
+     */
+    private CommandLine parseCommandLineOptions(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        Options options = initializeCommandlineOptions();
+
+        return parser.parse(options, args);
+    }
+
+    /**
+     * Initialize the default CLI option configurations.
+     *
+     * @return A list of CLI options.
+     */
+    private Options initializeCommandlineOptions() {
+        Options commands = new Options();
+
+        //Add task
+        Option addTask = new Option("new", true, "Create a new task. Optionally start the task.");
+        addTask.setArgs(2);
+        addTask.setArgName("new");
+
+        //Start existing task
+        Option startTask = new Option("start", true, "Start an existing task.");
+        startTask.setArgs(1);
+        startTask.setArgName("start");
+
+        //Stop existing task
+        Option stopTask = new Option("stop", false, "Stop a task that has already been started.");
+        stopTask.setArgName("stop");
+
+        //Remove task
+        Option removeTask = new Option("remove", true, "Remove (delete) a task from the task manager.");
+        removeTask.setArgs(1);
+        removeTask.setArgName("remove");
+
+        //List tasks
+        Option listTasks = new Option("list", false, "List all tasks with the length of time spent on each.");
+        listTasks.setArgName("list");
+
+        //Help formatter
+        Option help = new Option("help", false, "List command line options.");
+        help.setArgName("help");
+
+        commands.addOption(addTask);
+        commands.addOption(removeTask);
+        commands.addOption(startTask);
+        commands.addOption(stopTask);
+        commands.addOption(listTasks);
+        commands.addOption(help);
+
+        return commands;
+    }
+
+    /**
+     * Print CLI options to terminal.
+     *
+     * @param options - A list of command line options.
+     */
+    private void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("Task Manager", options);
+        System.out.println("\n\n");
+    }
+
+    /**
+     * Exit the program with a message.
+     *
+     * @param message - The message to be rendered in the console.
+     */
+    private void die(String message) {
+        System.out.println(message);
+        System.exit(0);
+    }
+
+    /**
+     * Exit the program with a list of all available CLI options.
+     */
+    private void dieWithHelper() {
+        printHelp(this.options);
+        System.exit(0);
+    }
+
+    /**
+     * Exit the program with a message and list all available CLI options.
+     *
+     * @param message - The message to be rendered in the console.
+     */
+    private void dieWithHelper(String message) {
+        System.out.println(message);
+        printHelp(this.options);
+        System.exit(0);
+    }
+}
